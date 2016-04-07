e2685168df42f610ad77d0d48c483c9cfd0f2af3
AAron <AAron@Cobo-PC>
2016-04-07 11:11:30 +0200

    FINALIZADO

---
+++ src/boletin_24/Boletin_24.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_24;
+
+/**
+ *
+ * @author AAron
+ */
+public class Boletin_24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Ventana ob = new Ventana();
+      ob.setVisible(true);
+      //Ventana2 ob2 = new Ventana2();
+      //ob2.setVisible(true);
+    }
+    
+}
